3925caf6125dacb71cb18c88ae503719b96d3c88
vadosichek <big-vadim@mail.ru>
2018-02-17 08:47:03 +0300

    Posts router #7

--- server/app/models.py
+++ server/app/models.py
@@ -1,8 +1,9 @@
 from app import db
 
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    login = db.Column(db.String(64), unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(64), unique=True)
     password = db.Column(db.String(128))
     name = db.Column(db.String(64))
     bio = db.Column(db.String(256))
@@ -10,17 +11,22 @@ class User(db.Model):
     pic = db.Column(db.String(32))
 
     def get(self):
-        return {'id':self.id,
-                'name':self.name,
-                'pic':self.pic}
+        return {'id': self.id,
+                'name': self.name,
+                'pic': self.pic}
+
     def profile(self):
-        return {'bio':self.bio,
-                'url':self.url}
+        return {'bio': self.bio,
+                'url': self.url}
+
+    def posts(self):
+        posts = Tour.query.filter_by(user_id=self.id)
+        return list(map(lambda x: {'id': x.id, 'pic': x.pic}, posts))
 
 
 class Tour(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    path = db.Column(db.String(32), unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    path = db.Column(db.String(32), unique=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     geotag = db.Column(db.String(64))
     desc = db.Column(db.Text)
@@ -33,29 +39,31 @@ class Tour(db.Model):
         return self.pic
 
     def get(self):
-        return {'pic':self.pic,
-                'desc':self.desc,
-                'tags':self.tags,
-                'time':self.time,
-                'geotag':self.geotag}
+        return {'pic': self.pic,
+                'desc': self.desc,
+                'tags': self.tags,
+                'time': self.time,
+                'geotag': self.geotag}
 
     def comments(self):
         comments = Comment.query.filter_by(tour_id=self.id)
-        return list(map(lambda x: {'user':x.user_id, 'text':x.text}, comments))
+        return list(map(lambda x: {'user': x.user_id, 'text': x.text}, comments))
 
 
 class Comment(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     tour_id = db.Column(db.Integer, db.ForeignKey('tour.id'))
     text = db.Column(db.String(128))
 
+
 class Like(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     tour_id = db.Column(db.Integer, db.ForeignKey('tour.id'))
 
+
 class Subscription(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     subscriber_id = db.Column(db.Integer, db.ForeignKey('user.id'))

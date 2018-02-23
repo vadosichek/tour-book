517acb7ebc3d7133adbef2353e8726baea4108e0
vadosichek <big-vadim@mail.ru>
2018-02-23 23:12:27 +0300

    Heroku config

--- server/config.py
+++ server/config.py
@@ -1,6 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'mysql://root:7710024vV@localhost:3306/tour_book'
+SQLALCHEMY_DATABASE_URI = 'mysql://b46fb4bee29661:8ebf584a@us-cdbr-iron-east-05.cleardb.net/heroku_1d7008e5f5e06f4'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'dp_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
